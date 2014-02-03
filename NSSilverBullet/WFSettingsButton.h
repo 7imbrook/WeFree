cd7f3241cb2fe8db9a22cc90e7817fcec8742c8d
Brandon Littell <brandonl3737@gmail.com>
2014-02-02 19:35:11 -0500

    Added nav bar and settings button.

---
+++ NSSilverBullet/WFSettingsButton.h
@@ -0,0 +1,13 @@
+//
+//  WFSettingsButton.h
+//  NSSilverBullet
+//
+//  Created by Brandon Littell on 2/2/14.
+//  Copyright (c) 2014 AppChallenge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WFSettingsButton : UIButton
+
+@end
